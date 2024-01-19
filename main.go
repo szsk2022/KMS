a49f05e2801f5b4ee9237690101ed9cab655aa83
szsk <admin@sunzishaokao.com>
2024-01-19 11:46:40 +0800

    1

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello,World!")
-	myprint("133")
+	getSum(1, 2)
 }
 func getSum(nums ...int) {
-
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(nums[i])
+		sum = sum + nums[i]
+	}
+	fmt.Println("sum:", sum)
 }
