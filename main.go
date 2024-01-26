e7f0db9c3a56ea1a669f5f1a248e525e508b5c77
szsk <admin@sunzishaokao.com>
2024-01-26 19:13:01 +0800

    1

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	arr := [4]int{1, 2, 3, 4}
-	fmt.Println(arr)
+	arr := []int{1, 2, 3, 4}
+	fmt.Println("默认：", arr)
 	update(arr)
+	fmt.Println("调用后的：", arr)
 }
-func update(arr2 [4]int) {
+func update(arr2 []int) {
+	fmt.Println("传递的：", arr2)
 	arr2[0] = 2
 	fmt.Println("修改后的", arr2)
 }
